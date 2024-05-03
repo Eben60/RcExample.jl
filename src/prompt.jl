fdf98342407a4f51571937b05138a6d504369269
Ben Elkin <8xe8n-cm@online.de>
2024-05-03 21:41:15 +0200

    WIP OK

---
+++ src/prompt.jl
@@ -0,0 +1,16 @@
+using RelaxationExample
+using GivEmExel, GivEmExel.SavingResults, GivEmExel.InternalArgParse
+
+fname = splitpath(@__FILE__)[end]
+
+include("prompt-init.jl")
+
+
+# Macni2020M1 RelaxationExample.jl % julia --project=. src/prompt2.jl -e --plotformat none
+
+pps = (;gen_options, spec_options, exelfile_prompt, next_file)
+
+fn(; kwargs...) = proc_n_save(procwhole, procsubset; kwargs...)
+
+fi = full_interact(pp0, pps, fn; getexel=true, getdata=(; dialogtype = :none))
+
